7a10d8269857edafa2931c10e03e8a56807343e4
Ju, Gyeongmin <46707197+amos42@users.noreply.github.com>
2025-12-03 20:21:52 +0900

    Update tool_test.py

--- examples/python/tool_test.py
+++ examples/python/tool_test.py
@@ -1,87 +1,43 @@
+#%%capture --no-stderr
+#%pip install python-dotenv bs4 langchain langchain-community langchain-openai
+
+
 import os
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain.agents import create_agent
+from langchain.messages import AIMessage
 
 load_dotenv()
 
-from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(model=os.getenv("OPENAI_API_MODEL"))
-
-from langchain_core.tools import tool
-from operator import attrgetter
-
 @tool
-def operator_xyz(a: int, b: int) -> int:
-    """operate xzy a and b.
+def operator_sharp(a: int, b: int) -> int:
+    """calcurate a # b.
 
     Args:
         a: first int
         b: second int
     """
-    print(f"call by llm operator xyz({a}, {b})")
+    #print(f"call by llm operator # ({a}, {b})")
     return a * b
 
-llm_with_tools = llm.bind_tools([operator_xyz])
-result = llm_with_tools.invoke("2과 3를 xyz 연산하면 몇이야?")
-print(result)
+llm = ChatOpenAI(openai_api_base=os.getenv("OPENAI_API_BASE"), model_name=os.getenv("OPENAI_API_MODEL"))
+result = llm.invoke("2 # 3 값은 몇이야? 한글로 답해 줘")
+print("[tool 없을 때]")
+print(result.content)
 
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
-prompt = ChatPromptTemplate.from_messages([
-    ("user", "{input}")
-])
+print("============================")
 
-template = '''Answer the following questions as best you can. You have access to the following tools:
+tools = [operator_sharp]
+agent = create_agent(llm, tools=tools)
 
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}'''
-prompt = PromptTemplate.from_template(template)
-
-# from langchain.agents import create_openai_functions_agent
-# #chain = prompt | llm_with_tools | attrgetter("tool_calls") | operator_xyz.map()
-# chain = create_openai_functions_agent(llm, [llm_with_tools], prompt)
-# #result = chain.invoke("2과 3를 xyz 연산하면 몇이야?")
-# result = chain.invoke({"input": "2과 3를 xyz 연산하면 몇이야?"})
-# #result = chain.invoke("안녕")
+result = agent.invoke({"messages" : ["2 # 3 값은 몇이야?", "한글로 답해 줘"]})
 # print(result)
 
-from langchain.agents import Tool, create_react_agent, AgentExecutor, AgentType
-
-# 에이전트 초기화
-agent = create_react_agent (
-    llm,
-    tools=[operator_xyz],  # 커스텀 도구를 리스트에 추가
-    prompt=prompt
-)
-
-tools = [
-    Tool(
-        name="operator xyz",
-        func=operator_xyz.run,
-        description=operator_xyz.__doc__,
-        verbose=True
-    )
-]
-
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=[operator_xyz],
-    verbose=True,
-    return_intermediate_steps=True,
-)
-
-result = agent_executor.invoke({"input": "2과 3를 xyz 연산하면 몇이야?"})
-print(result, end="\n")
+content = ""
+for msg in result["messages"]:
+    if type(msg) is AIMessage:
+        if msg.content: content += msg.content + "\n"
+print("[tool 있을 때]")
+print(content)
